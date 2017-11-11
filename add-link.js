b2ca023d8fb92aa0cffef664d2b02aab59edca2c
baurine <2008.hbl@gmail.com>
2017-11-11 17:45:56 +0800

    BUG #1 - Fix "View Gitbook" button disappear when page content changes by pajax

--- add-link.js
+++ add-link.js
@@ -3,9 +3,23 @@ function getHost() {
   return "http://quickgitbook.com"
 }
 
-function main() {
-  console.log('Log from quick-gitbook extension')
+// https://stackoverflow.com/questions/2844565/is-there-a-javascript-jquery-dom-change-listener
+// https://developer.mozilla.org/zh-CN/docs/Web/API/MutationObserver
+// http://www.jianshu.com/p/b5c9e4c7b1e1
+function listenDOMChange() {
+  var MutationObserver = window.MutationObserver || window.WebKitMutationObserver
+  var target = document.querySelector('#js-repo-pjax-container')
+  var observer = new MutationObserver(function(mutations) {
+    // mutations.forEach(function(mutation) {
+    //   console.log(mutation.type)
+    // })
+    addLink()
+  })
+  var config = { childList: true }
+  observer.observe(target, config)
+}
 
+function addLink() {
   // get repo name
   var repoLink = document.querySelector("h1.public strong[itemprop='name'] a")
   if (!repoLink) {
@@ -21,14 +35,22 @@ function main() {
   var repoActions = document.querySelector("ul.pagehead-actions")
   if (repoActions) {
     var link = document.createElement("a")
+    link.id = "viwe-gitbook"
+    link.classList = "btn btn-sm"
     link.href = getHost() + repoName
     link.target = "_blank"
     link.innerHTML = "View Gitbook"
-    link.classList = "btn btn-sm"
     var li = document.createElement("li")
     li.appendChild(link)
     repoActions.prepend(li)
   }
+}
+
+function main() {
+  console.log('Load quick-gitbook extension')
+
+  listenDOMChange()
+  addLink()
 }
 
 main()
