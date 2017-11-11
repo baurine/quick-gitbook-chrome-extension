546e5da28999060db850db7e4dbaa868792d49a1
baurine <2008.hbl@gmail.com>
2017-11-11 19:55:24 +0800

    FEATURE #2 - Only show "View Gitbook" link for gitbook repo
    - by check whether has "SUMMARY.md" in repo home page

--- add-link.js
+++ add-link.js
@@ -3,27 +3,47 @@ function getHost() {
   return "http://quickgitbook.com"
 }
 
+function log(msg) {
+  console.log('QuickGitbook extension:', msg)
+}
+
 // https://stackoverflow.com/questions/2844565/is-there-a-javascript-jquery-dom-change-listener
 // https://developer.mozilla.org/zh-CN/docs/Web/API/MutationObserver
 // http://www.jianshu.com/p/b5c9e4c7b1e1
 function listenDOMChange() {
-  var MutationObserver = window.MutationObserver || window.WebKitMutationObserver
   var target = document.querySelector('#js-repo-pjax-container')
+  if (!target) return
+  var MutationObserver = window.MutationObserver || window.WebKitMutationObserver
   var observer = new MutationObserver(function(mutations) {
     // mutations.forEach(function(mutation) {
     //   console.log(mutation.type)
     // })
-    addLink()
+    prepareAddLink()
   })
   var config = { childList: true }
   observer.observe(target, config)
+}
+
+function prepareAddLink() {
+  if (repoIsGitbook === UNKNOWN) checkGitbookRepo()
+  if (repoIsGitbook === YES) addLink()
+}
+
+// check whether this repo is a gitbook repo
+// according whether "SUMMARY.md" exists in repo home page
+function checkGitbookRepo() {
+  var isHomePage = document.querySelector("div.js-repo-meta-container") !== null
+  if (!isHomePage) return
+
+  var summaryRow = document.querySelector("tr.js-navigation-item td.content a[title='SUMMARY.md']")
+  repoIsGitbook = summaryRow ? YES : NO
 }
 
 function addLink() {
   // get repo name
   var repoLink = document.querySelector("h1.public strong[itemprop='name'] a")
   if (!repoLink) {
-    console.log('This is not repo page!')
+    log('this is not a repo page')
     return
   }
   // repoLink.href example: "https://github.com/baurine/study-note"
@@ -33,24 +53,33 @@ function addLink() {
 
   // add link
   var repoActions = document.querySelector("ul.pagehead-actions")
-  if (repoActions) {
-    var link = document.createElement("a")
-    link.id = "viwe-gitbook"
-    link.classList = "btn btn-sm"
-    link.href = getHost() + repoName
-    link.target = "_blank"
-    link.innerHTML = "View Gitbook"
-    var li = document.createElement("li")
-    li.appendChild(link)
-    repoActions.prepend(li)
+  if (!repoActions) {
+    log('there is no action buttons')
+    return
   }
+
+  // return in advance if link already exists
+  var existedLink = repoActions.querySelector("li#view-gitbook")
+  if (existedLink) return
+
+  var link = document.createElement("a")
+  link.id = "view-gitbook"
+  link.classList = "btn btn-sm"
+  link.href = getHost() + repoName
+  link.target = "_blank"
+  link.innerHTML = "View Gitbook"
+  var li = document.createElement("li")
+  li.appendChild(link)
+  repoActions.prepend(li)
 }
 
+var UNKNOWN = 0, NO = 1, YES = 2
+var repoIsGitbook = UNKNOWN
 function main() {
-  console.log('Load quick-gitbook extension')
+  log('load')
 
-  listenDOMChange()
-  addLink()
+  prepareAddLink()
+  if (repoIsGitbook !== NO) listenDOMChange()
 }
 
 main()
